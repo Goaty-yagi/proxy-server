2c0c1691a83f94f65ae0db7e4f2d784c036cde0c
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-06-02 15:20:36 +1000

    chore(tast): move corsheaders to the top in installed_app list

--- yelp_proxy/settings.py
+++ yelp_proxy/settings.py
@@ -51,13 +51,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
 
     'rest_framework',
 
